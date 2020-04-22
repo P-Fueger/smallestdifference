9efdaf5f8187728d09f0068676b19b2f2fa5139d
P-Fueger <i20fueger@gbs-technikum.de>
2020-04-22 12:58:38 +0200

    Test-Methoden sind komplett fertig!

--- testprojekt_ohne_maven/src/main/java/de/kohnlehome/Smallestdifference.java
+++ testprojekt_ohne_maven/src/main/java/de/kohnlehome/Smallestdifference.java
@@ -1,6 +1,10 @@
 package de.kohnlehome;
 
 public class Smallestdifference implements ISmallestdifference{
+
+    public Smallestdifference() {
+    }
+
     @Override
     public int difference(int[] n, int[] m) {
         return 3;
--- testprojekt_ohne_maven/src/test/java/de/kohnlehome/TestSmallestdifference.java
+++ testprojekt_ohne_maven/src/test/java/de/kohnlehome/TestSmallestdifference.java
@@ -1,6 +1,7 @@
 package de.kohnlehome;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -10,15 +11,38 @@ import static org.assertj.core.api.Assertions.*;
 public class TestSmallestdifference {
     public ISmallestdifference smallestdifference;
 
-
+    @BeforeEach
+    public void init(){
+        smallestdifference = new Smallestdifference();
+    }
 
     @Test
-    public void difference_1_3_15_11_2_mit_23_127_235_19_8(){
+    public void differenceTest_1_3_15_11_2_mit_23_127_235_19_8(){
         // ===== ACT =====
         int[] array1 = {1,3,15,11,2};
         int[] array2 = {23,127,235,29,8};
         int result = smallestdifference.difference(array1, array2);
         // ===== ASSERT =====
         assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    public void differenceTest_1_3_20_12_5_mit_23_127_235_19_8(){
+        // ===== ACT =====
+        int[] array1 = {1,3,20,12,6};
+        int[] array2 = {23,127,235,29,8};
+        int result = smallestdifference.difference(array1, array2);
+        // ===== ASSERT =====
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    public void differenceTest_1_3_234_11_2_mit_23_127_235_19_8(){
+        // ===== ACT =====
+        int[] array1 = {1,3,234,11,2};
+        int[] array2 = {23,127,235,29,8};
+        int result = smallestdifference.difference(array1, array2);
+        // ===== ASSERT =====
+        assertThat(result).isEqualTo(1);
     }
 }
